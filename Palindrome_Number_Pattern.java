95c197acb03778ab04182965e274b5a4d7d1266c
Navikesh S <150355065+Navikeshsaravanan@users.noreply.github.com>
2026-02-05 21:37:43 +0530

    Add files via upload

---
+++ Palindrome_Number_Pattern.java
@@ -0,0 +1,20 @@
+package project;
+import java.util.Scanner;
+
+public class Palindrome_Number_Pattern {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter no: ");
+        int num = scanner.nextInt();
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            for (int j=i-1;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+	}
+	
+}
