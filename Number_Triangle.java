1b60ce1a278ef695d1850d3f75d103adaf680fd4
Navikesh S <150355065+Navikeshsaravanan@users.noreply.github.com>
2026-02-07 09:58:41 +0530

    Create Number_Triangle.java

---
+++ Number_Triangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter no: ");
+            int num = scanner.nextInt();
+            for (int i =1;i<=num;i++){
+                for(int j=1;j<i+1;j++){
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+            scanner.close();
+
+        }
+}
