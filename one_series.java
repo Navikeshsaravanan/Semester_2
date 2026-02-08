51ca39f9431223a9b115712d5a939ff59bdaf53d
Navikesh S <150355065+Navikeshsaravanan@users.noreply.github.com>
2026-02-08 20:14:20 +0530

    Create one_series.java

---
+++ one_series.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter no : ");
+            int num = scanner.nextInt();
+            String sum = "0";
+            int total_sum=0;
+            for (int i = 0;i<=num;i++){
+                for (int j=0;j<i;j++){
+                    sum+="1";
+                }
+                int n = Integer.parseInt(sum);
+                total_sum+=n;
+                sum="";
+            }
+            System.out.println("The sum is: "+total_sum);
+        }
+}
