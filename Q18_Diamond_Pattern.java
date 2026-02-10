efb962ef51ff57cf886a814ae5f9960dafbc98c8
Navikesh S <150355065+Navikeshsaravanan@users.noreply.github.com>
2026-02-10 22:16:10 +0530

    Add files via upload

---
+++ Q18_Diamond_Pattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Q18_Diamond_Pattern {
+    public static void main(String[] args) {Scanner sc = new Scanner(System.in);
+        System.out.print("Enter height: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    }
+
