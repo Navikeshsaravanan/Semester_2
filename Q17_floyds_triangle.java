86f311df45cad8eafa8336095e7d36d59fa78e1b
Navikesh S <150355065+Navikeshsaravanan@users.noreply.github.com>
2026-02-26 22:47:35 +0530

    Add files via upload

---
+++ Q17_floyds_triangle.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Q17_floyds_triangle {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the no:");
+		int num = scanner.nextInt();
+		int i=0;
+		while(i<=num) {
+			for(int j=0;j<i;j++) {
+				int ps = (i+j)%2;
+				System.out.print(ps);
+			}
+			i++;
+			System.out.println();
+		}
+		scanner.close();
+	}
+}
